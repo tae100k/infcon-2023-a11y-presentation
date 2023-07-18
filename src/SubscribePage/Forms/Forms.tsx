edfef19025e08c416a62f38068ef82b4a71910eb
김태희 (Taehee Kim) <78027252+tae100k@users.noreply.github.com>
2023-07-19 01:31:29 +0900

    feat: forms section done

--- src/SubscribePage/Forms/Forms.tsx
+++ src/SubscribePage/Forms/Forms.tsx
@@ -1,19 +1,48 @@
-import {Box, Grid} from "@mui/material";
+import {Box} from "@mui/material";
 
 const Forms = () => {
   return (
     <Box
       sx={{
         display: "flex",
-        padding: "32px 148px 32px 0px",
+        padding: {
+          xs: "32px 40px 32px 0px",
+          md: "32px 74px 32px 0px",
+          lg: "32px 148px 32px 0px",
+        },
         alignItems: "flex-start",
         gap: "120px",
-        alignSelf: "stretch",
+        flexWrap: "wrap",
       }}
     >
-      <Box display="flex">
-        <Box>image</Box>
-        <Box>form</Box>
+      <Box
+        display="flex"
+        gap={"120px"}
+        sx={{
+          flexDirection: {xs: "column", md: "row"},
+          width: "100%",
+        }}
+      >
+        <Box
+          component="img"
+          src="images/three-people.jpg"
+          alt={"three people"}
+          loading="lazy"
+          sx={{
+            border: "1px solid black",
+            borderRadius: "0px 48px 48px 0px",
+            display: "flex",
+            width: "100%",
+            maxWidth: {md: "669px"},
+          }}
+        />
+        <Box
+          sx={{
+            padding: "32px 0px",
+          }}
+        >
+          form
+        </Box>
       </Box>
     </Box>
   );
