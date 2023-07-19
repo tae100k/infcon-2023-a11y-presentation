e1fc1d87f0414e4377f33bcdb6636cf9af750433
김태희 (Taehee Kim) <78027252+tae100k@users.noreply.github.com>
2023-07-20 01:43:20 +0900

    feat: suscribe forms done (#6)

--- src/SubscribePage/Forms/Forms.tsx
+++ src/SubscribePage/Forms/Forms.tsx
@@ -1,4 +1,6 @@
 import {Box} from "@mui/material";
+import {SubscribeForm} from "./SubscribeForm";
+import SubscribeImage from "./SubscribeImage";
 
 const Forms = () => {
   return (
@@ -10,39 +12,22 @@ const Forms = () => {
           md: "32px 74px 32px 0px",
           lg: "32px 148px 32px 0px",
         },
-        alignItems: "flex-start",
-        gap: "120px",
-        flexWrap: "wrap",
       }}
     >
       <Box
         display="flex"
-        gap={"120px"}
+        gap={{
+          xs: "40px",
+          md: "48px",
+          lg: "120px",
+        }}
         sx={{
-          flexDirection: {xs: "column", md: "row"},
+          flexDirection: {xs: "column", lg: "row"},
           width: "100%",
         }}
       >
-        <Box
-          component="img"
-          src="images/three-people.jpg"
-          alt={"three people"}
-          loading="lazy"
-          sx={{
-            border: "1px solid black",
-            borderRadius: "0px 48px 48px 0px",
-            display: "flex",
-            width: "100%",
-            maxWidth: {md: "669px"},
-          }}
-        />
-        <Box
-          sx={{
-            padding: "32px 0px",
-          }}
-        >
-          form
-        </Box>
+        <SubscribeImage />
+        <SubscribeForm />
       </Box>
     </Box>
   );
