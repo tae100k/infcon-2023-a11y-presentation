fee0981ee8d3e354756e12bd17f8d8eb61d35cc6
김태희 (Taehee Kim) <78027252+tae100k@users.noreply.github.com>
2023-07-25 01:00:20 +0900

    feat: imageList grid done

--- src/CollectionPage/Gallery/ImageList.tsx
+++ src/CollectionPage/Gallery/ImageList.tsx
@@ -1,8 +1,16 @@
-import { ImageList, ImageListItem } from "@mui/material";
+import {ImageList, ImageListItem} from "@mui/material";
 
 export const ImageLists = () => {
   return (
-    <ImageList variant="quilted" cols={4} rowHeight={121}>
+    <ImageList
+      variant="quilted"
+      cols={4}
+      rowHeight={121}
+      gap={25}
+      sx={{
+        overflow: "hidden",
+      }}
+    >
       {itemData.map((item) => (
         <ImageListItem
           key={item.img}
@@ -13,6 +21,9 @@ export const ImageLists = () => {
             {...srcset(item.img, 121, item.rows, item.cols)}
             alt={item.title}
             loading="lazy"
+            style={{
+              border: "2px solid var(--black, #000)",
+            }}
           />
         </ImageListItem>
       ))}
@@ -24,60 +35,38 @@ export const itemData = [
   {
     img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
     title: "Breakfast",
-    rows: 2,
-    cols: 2,
+    rows: 4,
+    cols: 1,
   },
   {
     img: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
     title: "Burger",
+    rows: 2,
+    cols: 2,
   },
   {
     img: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
     title: "Camera",
+    rows: 3,
+    cols: 1,
   },
   {
     img: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
     title: "Coffee",
-    cols: 2,
-  },
-  {
-    img: "https://images.unsplash.com/photo-1533827432537-70133748f5c8",
-    title: "Hats",
-    cols: 2,
-  },
-  {
-    img: "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62",
-    title: "Honey",
-    author: "@arwinneil",
-    rows: 2,
-    cols: 2,
+    rows: 4,
+    cols: 1,
   },
   {
     img: "https://images.unsplash.com/photo-1516802273409-68526ee1bdd6",
     title: "Basketball",
+    rows: 3,
+    cols: 2,
   },
   {
     img: "https://images.unsplash.com/photo-1518756131217-31eb79b20e8f",
     title: "Fern",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1597645587822-e99fa5d45d25",
-    title: "Mushrooms",
     rows: 2,
-    cols: 2,
-  },
-  {
-    img: "https://images.unsplash.com/photo-1567306301408-9b74779a11af",
-    title: "Tomato basil",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1471357674240-e1a485acb3e1",
-    title: "Sea star",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1589118949245-7d38baf380d6",
-    title: "Bike",
-    cols: 2,
+    cols: 1,
   },
 ];
 
