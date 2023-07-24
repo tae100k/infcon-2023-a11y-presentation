2a357d65ac4faeba4791a10082ffeb15e703b818
김태희 (Taehee Kim) <78027252+tae100k@users.noreply.github.com>
2023-07-25 01:09:41 +0900

    feat: gallery details done

--- src/CollectionPage/Gallery/ImageList.tsx
+++ src/CollectionPage/Gallery/ImageList.tsx
@@ -4,11 +4,11 @@ export const ImageLists = () => {
   return (
     <ImageList
       variant="quilted"
-      cols={4}
+      cols={20}
       rowHeight={121}
       gap={25}
       sx={{
-        overflow: "hidden",
+        overflow: "visible",
       }}
     >
       {itemData.map((item) => (
@@ -23,6 +23,7 @@ export const ImageLists = () => {
             loading="lazy"
             style={{
               border: "2px solid var(--black, #000)",
+              borderRadius: item.borderRadius,
             }}
           />
         </ImageListItem>
@@ -36,37 +37,43 @@ export const itemData = [
     img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
     title: "Breakfast",
     rows: 4,
-    cols: 1,
+    cols: 5,
+    borderRadius: "48px 0px",
   },
   {
     img: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
     title: "Burger",
     rows: 2,
-    cols: 2,
+    cols: 10,
+    borderRadius: "0px 48px",
   },
   {
     img: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
     title: "Camera",
     rows: 3,
-    cols: 1,
+    cols: 5,
+    borderRadius: "0px 48px",
   },
   {
     img: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
     title: "Coffee",
     rows: 4,
-    cols: 1,
+    cols: 5,
+    borderRadius: "48px 0px",
   },
   {
     img: "https://images.unsplash.com/photo-1516802273409-68526ee1bdd6",
     title: "Basketball",
     rows: 3,
-    cols: 2,
+    cols: 10,
+    borderRadius: "48px 0px",
   },
   {
     img: "https://images.unsplash.com/photo-1518756131217-31eb79b20e8f",
     title: "Fern",
     rows: 2,
-    cols: 1,
+    cols: 5,
+    borderRadius: "0px 48px",
   },
 ];
 
