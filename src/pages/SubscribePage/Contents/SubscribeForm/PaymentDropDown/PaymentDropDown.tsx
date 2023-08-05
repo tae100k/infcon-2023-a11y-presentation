8a56b9b1de84f98a77954d1bc96607f28fdfc7f2
김태희 (Taehee Kim) <78027252+tae100k@users.noreply.github.com>
2023-08-05 23:54:59 +0900

    feat: selectable with keyboard event

--- src/pages/SubscribePage/Contents/SubscribeForm/PaymentDropDown/PaymentDropDown.tsx
+++ src/pages/SubscribePage/Contents/SubscribeForm/PaymentDropDown/PaymentDropDown.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useRef, useEffect} from "react";
 import "./PaymentDropDown.css";
 import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
@@ -16,14 +16,51 @@ export const PaymentDropDown: React.FC<PaymentDropDownProps> = ({
   onSelectItem,
 }) => {
   const [isExpanded, setIsExpanded] = useState(false);
+  const buttonRef = useRef<HTMLButtonElement>(null);
+
+  useEffect(() => {
+    if (buttonRef.current) {
+      buttonRef.current.focus();
+    }
+  }, []);
+
   const toggleDropdown = () => setIsExpanded(!isExpanded);
+
   const iconWrapperClass = isExpanded
     ? "icon-wrapper expanded"
     : "icon-wrapper";
 
+  const handleKeyEvent = (event: React.KeyboardEvent<HTMLButtonElement>) => {
+    if (event.key === "Enter" || event.key === " ") {
+      toggleDropdown();
+      event.preventDefault();
+    }
+    if (event.key === "Escape" && isExpanded) {
+      toggleDropdown();
+      event.currentTarget.focus();
+    }
+  };
+
+  const handleItemKeyEvent = (event: React.KeyboardEvent, index: number) => {
+    if (event.key === "Enter" || event.key === " ") {
+      onSelectItem(index);
+      toggleDropdown();
+      event.preventDefault();
+    }
+  };
+
   return (
     <div className="dropdown">
-      <button className="dropdown-button" onClick={toggleDropdown}>
+      <button
+        className="dropdown-button"
+        onClick={toggleDropdown}
+        tabIndex={0}
+        ref={buttonRef}
+        onKeyDown={handleKeyEvent}
+        aria-label="Payment Dropdown"
+        aria-haspopup="listbox"
+        aria-expanded={isExpanded}
+      >
         <div className="dropdown-selected">{options[selectedItem]}</div>
         <div className={iconWrapperClass}>
           {isExpanded ? (
@@ -34,7 +71,7 @@ export const PaymentDropDown: React.FC<PaymentDropDownProps> = ({
         </div>
       </button>
       {isExpanded && (
-        <div className="dropdown-menu">
+        <div className="dropdown-menu" role="listbox">
           {options.map((option, index) => (
             <div
               key={`dropdown-option-${index}`}
@@ -45,6 +82,10 @@ export const PaymentDropDown: React.FC<PaymentDropDownProps> = ({
                 onSelectItem(index);
                 toggleDropdown();
               }}
+              onKeyDown={(event) => handleItemKeyEvent(event, index)}
+              tabIndex={0}
+              role="option"
+              aria-selected={selectedItem === index}
             >
               {option}
               {index === selectedItem && (
