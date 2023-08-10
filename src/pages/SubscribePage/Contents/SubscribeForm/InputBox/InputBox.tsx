e73e7d80146b0311717c35e2cd2505114bee3323
김태희 (Taehee Kim) <78027252+tae100k@users.noreply.github.com>
2023-08-11 01:19:45 +0900

    feat: form alert, form detailAddress added

--- src/pages/SubscribePage/Contents/SubscribeForm/InputBox/InputBox.tsx
+++ src/pages/SubscribePage/Contents/SubscribeForm/InputBox/InputBox.tsx
@@ -1,11 +1,12 @@
 import {Box} from "@mui/material";
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import "./InputBox.css";
 
 interface InfoInputProps {
   id: string;
   name?: string;
   type?: string;
+  value?: string;
   placeholder?: string;
   onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
   required?: boolean;
@@ -17,11 +18,16 @@ export const InputBox: React.FC<InfoInputProps> = ({
   name,
   type = "text",
   placeholder,
+  value,
   onChange,
   required,
   autoComplete,
 }) => {
-  const [inputValue, setInputValue] = useState("");
+  const [inputValue, setInputValue] = useState(value || "");
+
+  useEffect(() => {
+    setInputValue(value || "");
+  }, [value]);
   const [isFocused, setIsFocused] = useState(false);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
