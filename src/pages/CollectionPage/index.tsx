bbc2447bc5f9ae2cec08242cfd51b65e10e9a8a5
김태희 (Taehee Kim) <78027252+tae100k@users.noreply.github.com>
2023-08-13 20:47:59 +0900

    feat: header added

--- src/pages/CollectionPage/index.tsx
+++ src/pages/CollectionPage/index.tsx
@@ -1,12 +1,14 @@
-import { Box } from "@mui/material";
-import { Cards } from "shared/Cards/Cards";
-import { Copyright } from "shared/Copyright/Copyright";
-import { Header } from "./Header/Header";
-import { Gallery } from "./Gallery/Gallery";
+import {Box} from "@mui/material";
+import {Cards} from "shared/Cards/Cards";
+import {Copyright} from "shared/Copyright/Copyright";
+import {Header} from "./Header/Header";
+import {Gallery} from "./Gallery/Gallery";
+import {Navbar} from "shared/Navbar/Navbar";
 
 const CollectionPage = () => {
   return (
-    <Box bgcolor={"#e7e2df"} pt={"32px"} pb={"64px"} height={"100%"}>
+    <Box bgcolor={"#e7e2df"} pb={"64px"} height={"100%"}>
+      <Navbar />
       <Header />
       <Gallery />
       <Cards />
--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -2,11 +2,13 @@ import {Box} from "@mui/material";
 import {Carousels} from "pages/MainPage/Carousel/Carousel";
 import {Cards} from "shared/Cards/Cards";
 import {Copyright} from "shared/Copyright/Copyright";
+import {Navbar} from "shared/Navbar/Navbar";
 import {Header} from "./Header/Header";
 
 const MainPage = () => {
   return (
-    <Box bgcolor={"#e7e2df"} pt={"32px"} pb={"64px"} height={"100%"}>
+    <Box bgcolor={"#e7e2df"} pb={"64px"} height={"100%"}>
+      <Navbar />
       <Header />
       <Carousels />
       <Cards />
